e7e6a4efd9a3d5fe7bfe2777be96ece4fcdfb161
James T Ardian <jamesardian01@gmail.com>
2022-01-05 17:16:53 -0800

    added req.session.destroy to clear cookie more certainly

--- server/controller/signOutController.js
+++ server/controller/signOutController.js
@@ -2,6 +2,7 @@ module.exports = {
     logOutUser: (req, res)=>{
         if(req.session.user){
             // Cookie begone
+            req.session.destroy();
             res.clearCookie("username")
         
             res.status(200).json({loggedOut: true, msg: "User has been logged out"})
